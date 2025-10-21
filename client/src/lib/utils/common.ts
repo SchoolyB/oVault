a896bf4a6a17b40ba2a2a6b6667c38ca9449177e
SchoolyB <marshallbcodes@gmail.com>
2025-10-20 21:34:05 -0500

    chore: add more types, constants & helpers to utils

--- client/src/lib/utils/common.ts
+++ client/src/lib/utils/common.ts
@@ -1,3 +1,11 @@
+//Constant
+export const BASE_URL = 'http://localhost:8042/api/v1';
+export const CREDENTIAL_COLLECTION = "/projects/secure/collections/credentials"
+export const CREDENTIAL_CLUSTER = "/projects/secure/collections/credentials/clusters/creds"
+export const USERNAME_RECORD = "/projects/secure/collections/credentials/clusters/creds/records/username"
+export const PASSWORD_RECORD = "/projects/secure/collections/credentials/clusters/creds/records/passwords"
+
+
 //Types
 export interface AccountEntry {
 	id: string;
@@ -31,4 +39,51 @@ export interface GeneratedPasswordOptions {
 	includeLowercase: boolean;
 	includeNumbers: boolean;
 	includeSymbols: boolean;
+}
+
+export interface OstrichDBRecord {
+		id: string;
+		name: string;
+		type: string;
+		value: string;
+	}
+
+	export enum RequestMethod{
+    GET = 0,
+    POST = 1,
+    DELETE  = 2,
+    PUT = 3
+	}
+
+	//Just a helper to get str val of enum. TS aint so bad i guess.
+export function handle_method(reqType: RequestMethod): string {
+  switch (reqType) {
+    case RequestMethod.GET:
+      return "GET";
+    case RequestMethod.POST:
+      return "POST";
+    case RequestMethod.DELETE:
+      return "DELETE";
+    case RequestMethod.PUT:
+      return "PUT";
+    default:
+      throw new Error('Unhandled method passed');
+  }
+}
+
+	export async function handle_request(reqType: RequestMethod, url: string, token: string | undefined): Promise<Response> {
+
+	const response = await fetch(
+		`${BASE_URL}${url}`,
+		{
+			method: handle_method(reqType),
+			headers: {
+				Authorization: `Bearer ${token}`,
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			}
+		}
+	);
+
+	return response
 }
